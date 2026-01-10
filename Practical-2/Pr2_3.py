4af07a8fb0641738593891c71fe3f1f162167f9b
Nishit079 <pnishit004@gmail.com>
2026-01-10 16:51:23 +0530

    Create Pr2_3.py

---
+++ Practical-2/Pr2_3.py
@@ -0,0 +1,13 @@
+#Write python programs to evaluate the following expressions to demonstrate the use of operator precedence and associativity.
+#12 + 3 - 4 / 2 < 3 + 13
+#X = (10 * 2) %  2 << 4++ 
+
+
+
+# Expression 1
+result1 = 12 + 3 - 4 / 2 < 3 + 13
+print("Result of expression 1:", result1)
+
+# Expression 2 
+X = (10 * 2) % 2 << 4
+print("Result of expression 2:", X)
