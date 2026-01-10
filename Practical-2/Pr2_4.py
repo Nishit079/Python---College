91ac9566f351b6a92ea7a7732c462e110d41defd
Nishit079 <pnishit004@gmail.com>
2026-01-10 16:52:01 +0530

    Create Pr2_4.py

---
+++ Practical-2/Pr2_4.py
@@ -0,0 +1,21 @@
+#Write a python program to display data types of different variables. 
+
+a = 79
+b = 7.9
+c = "Hello"
+d = False
+e = [1 , 2 , 3]
+f = (4,5,6)
+g = {"y": 2}
+h = {1,2,3} 
+i = None
+
+print("Data type of a =", type(a))
+print("Data type of b =", type(b))
+print("Data type of c =", type(c))
+print("Data type of d =", type(d))
+print("Data type of e =", type(e))
+print("Data type of f =", type(f))
+print("Data type of g =", type(g))
+print("Data type of h =", type(h))
+print("Data type of i =", type(i))
