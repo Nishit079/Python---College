8f53642652676c85d7c29766dd770d101f25a643
Nishit079 <pnishit004@gmail.com>
2026-01-10 16:51:16 +0530

    Create Pr2_2.py

---
+++ Practical-2/Pr2_2.py
@@ -0,0 +1,16 @@
+# Taking student information as 
+name = input("Enter Name: ")
+enrollment = input("Enter Enrollment Number: ")
+branch = input("Enter Branch: ")
+age = input("Enter Age: ")
+email = input("Enter Email: ")
+mobile = input("Enter Mobile Number: ")
+
+# Displaying student information
+print("\n-- Student Information --")
+print("Name           :", name)
+print("Enrollment No. :", enrollment)
+print("Branch         :", branch)
+print("Age            :", age)
+print("Email          :", email)
+print("Mobile No.     :", mobile)
